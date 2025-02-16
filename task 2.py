15340d36eb7f1eebe67091f9aa5ed9ec28c0e279
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-02-16 19:27:16 +0200

    add task 2

---
+++ task 2.py
@@ -0,0 +1,19 @@
+import random
+
+
+def get_numbers_ticket(min, max, quantity):
+    if min < 1:
+        raise ValueError("min should be greater than 1")
+
+    if max > 1000:
+        raise ValueError("max should be less than 1000")
+
+    if min > max:
+        raise ValueError("min should be less than max")
+
+    if quantity > max or quantity < min:
+        raise ValueError("quantity should be between min and max")
+
+    possible_numbers = list(range(min, max))
+
+    return sorted(random.sample(possible_numbers, quantity))
