021fcd5998b0024a409efb087fa07ef96b8421c6
Eugene Asaulyak <eugene.asaulyak@gmail.com>
2025-02-23 10:18:30 +0200

    fix: fix a corner case with bday over year

--- task-4.py
+++ task-4.py
@@ -8,10 +8,14 @@ def get_upcoming_birthdays(company_users):
     for user in company_users:
         birthday = datetime.strptime(user["birthday"], "%Y.%m.%d")
         birthday_this_year = datetime(year=today.year, month=birthday.month, day=birthday.day).date()
+        birthday_next_year = datetime(year=today.year + 1, month=birthday.month, day=birthday.day).date()
 
-        diff_days = (birthday_this_year - today).days
+        diff_days_this_year = abs((birthday_this_year - today).days)
+        diff_days_next_year = abs((birthday_next_year - today).days)
+        diff_days = min(diff_days_this_year, diff_days_next_year)
+
         if 0 <= diff_days < 7:
-            congratulation_date = birthday_this_year
+            congratulation_date = today + timedelta(days = diff_days)
             congratulation_date_week_day = congratulation_date.isoweekday()
 
             if congratulation_date_week_day in [6, 7]:
@@ -21,6 +25,3 @@ def get_upcoming_birthdays(company_users):
             upcoming_birthdays_this_week.append({'name': user["name"], 'congratulation_date': congratulation_date})
 
     return upcoming_birthdays_this_week
-
-
-
